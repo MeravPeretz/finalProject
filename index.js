2cb912632e71e5e5bb0ffaf021ad52f53e449e16
Your Name <73@>
2023-12-20 19:35:13 +0200

    שינוי חמישי של מירב

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 console.log("hey! im merav this is my change!");
 console.log("hey! im merav this is my second change!");
 
-console.log("hey! im tsipi this is my change!"+
-"and i change you-merav!");
+console.log("hey! im tsipi this is my change!" +
+    "and i change you-merav!");
 console.log("hey! im merav this is my third change!");
 
+//changes!
+const arr = [1, 2, 3, 4, 5, 6, 7]
+console.log(arr);
+//end changes!
 console.log(" merav-four change");
